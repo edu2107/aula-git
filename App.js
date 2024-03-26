094147361ace25ebb01d9f86458c96571ec9a1b4
unknown <you@example.com>
2024-03-26 14:08:17 -0300

    adição de botoes

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Text, StyleSheet, View, Image } from 'react-native';
+
+import Header from './components/header';
+
+import Card from './components/Card';
+
+export default function App() {
+
+return (
+
+<View style={estilo.container}>
+
+<Header></Header>
+
+<Card porcentagem = '6' titulo = 'TIME' imagem = 'https://media.santosfc.com.br/wp-content/uploads/2022/04/Asset-2.jpg' descricao = 'SANTOS'></Card>
+
+<Card porcentagem = '6' titulo = 'TIME' imagem = 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/2026.png' descricao = 'SÃO PAULO'></Card>
+
+<Card porcentagem = '6' titulo = 'TIME' imagem = 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/6079.png' descricao = 'BRAGANTINO'></Card>
+
+<Card porcentagem = '6' titulo = 'TIME' imagem = 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/18127.png' descricao = 'NOVORIZONTINO'></Card>
+
+<Card porcentagem = '6' titulo = 'TIME' imagem = 'https://s3.static.brasilescola.uol.com.br/be/2023/09/escudo-da-ponte-preta.jpg' descricao = 'PONTE PRETA'></Card>
+
+<Card porcentagem = '6' titulo = 'TIME' imagem = 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/4774.png' descricao = 'ITUANO'></Card>
+
+</View>
+
+);
+
+}
+
+const estilo = StyleSheet.create({
+
+container: {
+
+flex: 1,
+
+alignItems: 'center',
+
+backgroundColor: '#CAF0F8',
+
+borderColor: '#90E0EF',
+
+paddingHorizontal: 20,
+
+},
+
+});
---
+++ components/card/index.js
@@ -0,0 +1,29 @@
+import {Text, View, Image} from 'react-native'
+
+import Header from '../header';
+
+import estilo from './style';
+
+export default function Card({porcentagem,titulo,imagem,descricao}){
+
+return(
+
+< View style={estilo.cardTarefa}>
+
+<Image style={estilo.imagemCard} source={{ uri:imagem }} />
+
+<View style={{ marginLeft: 15,width:150 }}>
+
+<Text style={estilo.tituloCard}>{titulo}</Text>
+
+<Text style={estilo.textoCard}>{descricao}</Text>
+
+</View>
+
+<Text style={{ fontSize: 25 }}>{porcentagem}</Text>
+
+</View>
+
+);
+
+}
