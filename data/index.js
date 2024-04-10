4f0f3316efa098f8271291267f33e523cc17c969
edu2107 <165069430+edu2107@users.noreply.github.com>
2024-04-10 16:12:11 -0300

    index.js

--- data/index.js
+++ data/index.js
@@ -1,49 +1,49 @@
 const data = [
 {
     id:'1',
-    porcentagem:'',
-    titulo:'',
-    imagem:'',
-    descriçao:'',
+    porcentagem:'10%',
+    titulo:'jogar o lixo',
+    imagem:'https://w7.pngwing.com/pngs/789/70/png-transparent-throw-garbage-dustbin-man-throwing-clean-trash-glyph-icon.png',
+    descriçao:'jogo o lixo de casa',
 },
 
 {
     id:'2',
-    porcentagem:'',
-    titulo:'',
-    imagem:'',
-    descriçao:'',
+    porcentagem:'65%',
+    titulo:'Academia',
+    imagem:'https://i.pinimg.com/236x/6c/15/9b/6c159b8b6934273b47dc94edf0c566b7.jpg',
+    descriçao:'Ir para a academia',
 },
 
 {
     id:'3',
-    porcentagem:'',
-    titulo:'',
-    imagem:'',
-    descriçao:'',
+    porcentagem:'5%',
+    titulo:'Alimentação',
+    imagem:'https://www.designi.com.br/images/preview/10005128.jpg',
+    descriçao:'me alimentar',
 },
 
 {
     id:'4',
-    porcentagem:'',
-    titulo:'',
-    imagem:'',
-    descriçao:'',
+    porcentagem:'10%',
+    titulo:'Basquete',
+    imagem:'https://img1.gratispng.com/20180802/srq/kisspng-michael-jordan-chicago-bulls-nba-birmingham-barons-michael-jordan-chicago-bulls-basketball-custom-pri-5b631c8507d282.208111031533222021032.jpg',
+    descriçao:'Jogar Basquete',
 },
 
 {
     id:'5',
-    porcentagem:'',
-    titulo:'',
-    imagem:'',
-    descriçao:'',
+    porcentagem:'5%',
+    titulo:'Soltar Pipa',
+    imagem:'https://w7.pngwing.com/pngs/627/351/png-transparent-kite-cartoon-illustration-small-people-flying-kite-material-free-to-pull-game-child-free-logo-design-template.png',
+    descriçao:'empinar pipa',
 },
 
 {
     id:'6',
-    porcentagem:'',
-    titulo:'',
-    imagem:'',
-    descriçao:'',
+    porcentagem:'75%',
+    titulo:'Tomar Banho',
+    imagem:'https://i.pinimg.com/564x/11/87/1b/11871b9fae0406f0668fdaed00a94fed.jpg',
+    descriçao:'Banho',
 },
 ]
